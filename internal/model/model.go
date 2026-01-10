8dd1b7f029e937e3698fc8fc9ab990e90dc93f27
saitbatalov.a2 <saitbatalov.a2@rwb.ru>
2026-01-10 18:57:04 +0300

    CRUD операции новостей

---
+++ internal/model/model.go
@@ -0,0 +1,30 @@
+package model
+
+import "time"
+
+type Item struct {
+	Title      string    `json:"title"`
+	Categories []string  `json:"categories"`
+	Link       string    `json:"link"`
+	Date       time.Time `json:"date"`
+	Summary    string    `json:"summary"`
+	SourceName string    `json:"source_name"`
+}
+
+type Source struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	FeedURL   string    `json:"feed_url"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type Article struct {
+	ID          int64     `json:"id"`
+	Title       string    `json:"title"`
+	Source      Source    `json:"source"`
+	Link        string    `json:"link"`
+	Summary     string    `json:"summary"`
+	PublishedAt time.Time `json:"published_at"`
+	PostedAt    time.Time `json:"posted_at"`
+	CreatedAt   time.Time `json:"created_at"`
+}
